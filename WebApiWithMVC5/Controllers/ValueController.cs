2981782d7f8db234c6c8cf7c9e899bb4068230d6
DELL <DELL@DESKTOP-SOHV4TH>
2023-04-25 11:28:22 +0530

    Add project files.

---
+++ WebApiWithMVC5/Controllers/ValueController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiWithMVC5.Controllers
+{
+    public class ValueController : ApiController
+    {
+        [Route("api/AshokAction/{id}")]
+        public IHttpActionResult Get([FromUri]int id)
+        {
+            if (id>5)
+            {
+                return Ok(1);
+            }
+            else
+            {
+                return NotFound();
+            }
+            
+        }
+    }
+}
---
+++ WebApiWithMVC5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApiWithMVC5.MvcApplication" Language="C#" %>
